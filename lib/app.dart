94b151762169e17f1fedb6ccc93328fff8222328
GiddyCode <ogbonnagideon1994@gmail.com>
2022-01-03 13:53:19 +0100

    ..

--- lib/app.dart
+++ lib/app.dart
@@ -13,7 +13,6 @@ import 'generated/i18n.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class App extends StatefulWidget {
-
   App({Key key}) : super(key: key);
 
   @override
