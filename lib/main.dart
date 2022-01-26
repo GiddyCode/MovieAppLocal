df888bba53a44326487c02f3e04f817836bfe662
GiddyCode <ogbonnagideon1994@gmail.com>
2022-01-26 23:45:58 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_downloader/flutter_downloader.dart';
 import 'app.dart';
 
 
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterDownloader.initialize();
